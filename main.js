66052fa25c9629242ec42656a65c55e62036d594
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2024-05-29 21:58:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+// create class
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class person {
+    students = [];
+    addStudent(object) {
+        this.students.push(object);
+    }
+}
+const persons = new person();
+const programStart = async (persons) => {
+    do {
+        console.log("Welcome!");
+        // await called out
+        const answer = await inquirer.prompt({
+            // create object
+            name: "select",
+            type: "list",
+            message: chalk.yellow("who would you like to interact with?"),
+            choices: ["staff", "student", "exit"],
+        });
+        // if else statement
+        if (answer.select == "staff") {
+            console.log("You approach the staff room. Please feel free to ask me any question");
+        }
+        else if (answer.select == "student") {
+            // user inpur
+            const answer = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: chalk.green("Enter the student's name you want to engage with:")
+            });
+            const student = persons.students.find(val => val.name == answer.student);
+            if (!student) {
+                const name = new Student(answer.student);
+                persons.addStudent(name);
+                console.log(`Hello I am ${name.name}. Nice to meet you!`);
+                console.log("New student added");
+                console.log("Current student list");
+                console.log(persons.students);
+            }
+            else {
+                console.log(`Hello I am ${student.name}. Nice to see you again!`);
+                console.log(" Yor are existing student list");
+                console.log(persons.students);
+            }
+        }
+        else if (answer.select == "exit") {
+            console.log("Exiting the program...");
+            process.exit();
+        }
+    } while (true);
+};
+programStart(persons);
